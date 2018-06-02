19f64967353262779abd85bf8cbf489253b32afd
Kamus <kamuschan@outlook.com>
2018-06-02 13:19:09 +0800

    database configuration

---
+++ app/src/main/java/com/zsc/edu/zscweather/db/County.java
@@ -0,0 +1,10 @@
+package com.zsc.edu.zscweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private  int cityId;
+}
